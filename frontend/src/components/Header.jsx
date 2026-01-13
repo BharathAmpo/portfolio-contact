c5f968d68710d13ef5cb5472fb3c1e2dee27e002
Bharath Kumar Ampolu <bharathkumarampolu@gmail.com>
2026-01-13 23:19:18 +0530

    implement header component structure

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,14 @@
+import styles from "./Header.module.css";
+
 function Header() {
-  return <header>Header</header>;
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <h1 className={styles.name}>Bharath Kumar Ampolu</h1>
+        <p className={styles.role}>Frontend Developer</p>
+      </div>
+    </header>
+  )
 }
+
 export default Header;
